fd0142e790d446f75fe27f3844dc0defe6f09f2e
Colin Hale <colinah@colins-mbp.lan>
2019-12-19 10:21:55 -0700

    Adding board locations

--- TicTacToe.js
+++ TicTacToe.js
@@ -2,4 +2,21 @@ const width = 400;
 
 const scl = 400/3
 
-const lines = [{x:scle,y:0}]
+function TTT() {
+    this.board = [['','',''],['','',''],['','','']]
+    this.boardLocations = [[{x:scl/2,y:scl/2},{x:scl+scl/2,y:scl/2},{x:scl*2+scl/2,y:scl/2}],
+                        [{x:scl/2,y:scl+scl/2},{x:scl+scl/2,y:scl+scl/2},{x:scl*2+scl/2,y:scl+scl/2}],
+                        [{x:scl/2,y:scl*2+scl/2},{x:scl+scl/2,y:2*scl+scl/2},{x:scl*2+scl/2,y:2*scl+scl/2}]];
+
+    this.draw = () => {
+        for(let i = 0; i < 3;i++){
+            for(let j = 0; j < 3; j++){
+
+            }
+        }
+    }
+
+
+
+
+}
--- sketch.js
+++ sketch.js
@@ -3,7 +3,11 @@ function setup() {
   }
   
   function draw() {
-      line()
+      stroke(4)
+      line(width/3,0,width/3,width);
+      line(0,width/3,width,width/3);
+      line((width/3)*2,0,(width/3*2),width);
+      line(0,(width/3)*2,width,(width/3*2));
       if(mouseIsPressed){
           fill(0)
 
